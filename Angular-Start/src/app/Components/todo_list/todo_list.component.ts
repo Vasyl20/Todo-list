e9f626e271f3b67e6fd14493597c4bc689bcb23d
MKA <mka@rivne.itstep.org>
2021-08-27 19:07:47 +0300

    Create logic of tables

--- Angular-Start/src/app/Components/todo_list/todo_list.component.ts
+++ Angular-Start/src/app/Components/todo_list/todo_list.component.ts
@@ -1,4 +1,4 @@
-import { asNativeElements, Component, OnInit } from '@angular/core';
+import { asNativeElements, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'todo_list',
@@ -7,18 +7,19 @@ import { asNativeElements, Component, OnInit } from '@angular/core';
 })
 export class Todo_listComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRaf:ElementRef) { }
 
+  add_a_group() {
+     var add_a_group = document.getElementById("dir-todo_list-id");
+    //  document.getElementById("dir-todo_list")?.append("<div class='group'></div>");
+
+
+    console.log("2");
+  }
 
 
   ngOnInit() {
 
-    function add_a_group() {
-      var add_a_group = document.getElementById("dir-todo_list-id");
-       document.getElementById("dir-todo_list")?.append("<div class='group'></div>")
-
-      console.log("2");
-    }
 
 
   }
--- Angular-Start/src/app/app.module.ts
+++ Angular-Start/src/app/app.module.ts
@@ -1,6 +1,10 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -16,8 +20,10 @@ import { Todo_listComponent } from './Components/todo_list/todo_list.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
